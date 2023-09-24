d911f61ded87ba6b43a880774a902ecba3aaf444
sanchiaki <sanchiaki@gmail.com>
2023-09-25 01:02:55 +0800

    https push done

--- src/main/java/com/example/jgit/test/App.java
+++ src/main/java/com/example/jgit/test/App.java
@@ -3,6 +3,7 @@ package com.example.jgit.test;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 /**
  * Hello world!
@@ -20,19 +21,10 @@ public final class App {
      * @throws InvalidRemoteException
      */
     public static void main(String[] args) throws InvalidRemoteException, TransportException, GitAPIException {
-        GitClient gitClient = new GitClient("git@github.com:SanChiaki/langchain-learning.git", "./langchain-learning");
+        GitClient gitClient = new GitClient("https://github.com/SanChiaki/langchain-learning.git", "./langchain-learning");
 
-        // try {
-        //     // Make changes to the repository
-        //     // ...
-
-        //     // Commit changes
-        //     gitClient.commitRepository("Commit message")    ;
-
-        //     // Push changes
-        //     gitClient.pushRepository();
-        // } catch (GitAPIException e) {
-        //     e.printStackTrace();
-        // }
+        // Set up credentials
+        gitClient.commitRepository("test");
+        gitClient.pushRepository();
     }
 }
